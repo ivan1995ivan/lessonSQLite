7d7b147908a6c83b678489717da1741f39288a4b
Ivan Zalozh <agriodessa@gmail.com>
2022-10-02 08:52:43 +0300

    end

--- lessonSQLite.py
+++ lessonSQLite.py
@@ -3,5 +3,20 @@ import sqlite3
 base = sqlite3.connect('new.db')
 cur = base.cursor()
 
-base.execute('CREATE TABLE data(login, pasword)')
+# base.execute('CREATE TABLE IF NOT EXISTS {}(login PRIMARY KEY, pasword)'.format('data'))
+# base.commit()
+
+# cur.execute('INSERT INTO data VALUES(?, ?)', ('jonny123', '123456789'))
+# base.commit()
+# cur.execute('INSERT INTO data VALUES(?, ?)', ('billy123', 'password'))
+# base.commit()
+
+# r = cur.execute('SELECT login FROM data').fetchall()
+# r = cur.execute('SELECT pasword FROM data WHERE login ==?',('jonny123',)).fetchone()
+# cur.execute('UPDATE data SET pasword == ? WHERE pasword == ?', ('123456789', 'password'))
+# base.commit()
+# cur.execute('DELETE FROM data WHERE login == ?', ('jonny123',))
+# base.commit()
+base.execute('DROP TABLE IF EXISTS data')
 base.commit()
+base.close()
